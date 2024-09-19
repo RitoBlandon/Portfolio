62cef6f1dd2a17516669b558f3e23a707f7ddd3f
RitoBlandon <112638717+RitoBlandon@users.noreply.github.com>
2024-09-19 13:10:20 -0400

    Ajout du stack de tim tools suite

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+!function(){"use strict";class t{constructor(){this.init()}init(){lottie.loadAnimation({container:document.getElementById("animation"),renderer:"svg",loop:!0,autoplay:!0,path:"/dist/assets/lottie/boutonprojets.json"})}}new class{constructor(){this.init()}init(){new t}}}();
